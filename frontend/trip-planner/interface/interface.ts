a3966f0b1a45b416b6c3a02019767110a2d475a6
Passawut <passawut.prinyagul@gmail.com>
2024-11-09 02:40:43 +0700

    feat: add interafaces

--- frontend/trip-planner/interface/interface.ts
+++ frontend/trip-planner/interface/interface.ts
@@ -1,4 +1,6 @@
-export default interface Records {
+// Old version
+
+export interface Records {
     _id: string,
     title: string,
     region: string,
@@ -9,7 +11,7 @@ export default interface Records {
     prompt: Location[]
 }
 
-export default interface Location {
+export interface Location {
     location: string,
     detail: string,
     sequence: number,
@@ -22,11 +24,101 @@ export default interface Location {
     timeTravelToNextLocation: string
 }
 
-export default interface MessageDto {
+// New Version
+
+export interface MessageDto {
     region: string;
     budget: string;
     tripStart: string;
     tripEnd: string;
     peopleNo: string;
     preferences: string[] | null;
+}
+
+export interface JwtPayload {
+    sub: string;
+    name: string;
+    username: string;
+    email: string;
+    iat: number;
+    exp: number;
+}
+
+export interface TripPlanDtoWithDetails{
+    details: MessageDto;
+
+    locations: LocationDto[];
+
+    hotels: HotelDto[];
+}
+
+export interface TripPlanDto {
+    locations: LocationDto[];
+
+    hotels: HotelDto[];
+}
+
+export interface ConfirmTripPlanDto{
+
+    locations: LocationDto[];
+
+    hotel: HotelDto | null; 
+}
+
+
+export interface LocationDto {
+    location: string;
+
+    detail: string;
+
+    sequence: number;
+
+    dateNo: number;
+
+    startDateTime: Date;
+
+    endDateTime: Date;
+
+    latitude: number;
+
+    longitude: number;
+
+    entryCost: string;
+
+    timeTravelToNextLocation: string;
+}
+  
+export interface HotelDto {
+    hotelName: string;
+
+    hotelAddress: string;
+
+    price: number;
+
+    imageUrl: string;
+
+    latitude: number;
+
+    longitude: number;
+
+    rating: number;
+
+    description: string;
+}
+
+export interface RecordDto{
+
+    title: string;
+  
+    region: string;
+  
+    budget: number;
+  
+    startDate: Date;
+  
+    endDate: Date;
+  
+    preference: string[]| null;
+
+    prompt: ConfirmTripPlanDto;
 }
